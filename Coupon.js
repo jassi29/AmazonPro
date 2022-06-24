20f6f83b6c5c9679685d867eb894156afb147ab2
Yogesh Kumar <92639956+jassi29@users.noreply.github.com>
2022-06-24 19:51:10 +0530

    b

---
+++ Coupon.js
@@ -0,0 +1,7 @@
+function loadCoupons(){
+    document.getElementById('coupon-container').style.visibility = 'visible';
+
+}
+function closeCoupon(){
+    document.getElementById('coupon-container').style.visibility = 'hidden';
+}
